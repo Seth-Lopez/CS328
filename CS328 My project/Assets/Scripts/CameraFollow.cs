5dcfe380c22cf01d02fb355c825ea2e9eaa366bc
Seth-Lopez <70123955+Seth-Lopez@users.noreply.github.com>
2023-11-17 00:47:56 -0800

    Completed NavMesh - Seth
    Completed NavMesh
    Player Movement Script
    Boarders
    Health and Energy Images

--- CS328 My project/Assets/Scripts/CameraFollow.cs
+++ CS328 My project/Assets/Scripts/CameraFollow.cs
@@ -7,15 +7,11 @@ public class CameraFollow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        public float Follow Speed = 2f;
-        public Transform tartget;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target,position.x,target.position.y,-10f);
-        transform.position = Vector3.Slerp(transform.position,newPos,FollowSpeed*Time.deltaTime);
-        
     }
 }
--- CS328 My project/Assets/Scripts/PlayerMovement.cs
+++ CS328 My project/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class PlayerMovement : MonoBehaviour
+using UnityEngine.UI;
+using UnityEngine.Animations;
+using Unity.Mathematics;
+public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // For Movement: 
+    [SerializeField] private float walkingSpeed = 5f;
+    [SerializeField] private float sprintSpeed;
+    private float currentMovementSpeed;
+    private Vector2 movementDirection;
+    // RigidBody 2D:
+    private Rigidbody2D rb;
+    // For Health:
+    private GameObject healthBarGameObject;
+    private Image healthBar;
+    [SerializeField] private float currentHealth = 100f;
+    private float maxHealth = 100;
+    // For Energy:
+    private GameObject energyBarGameObject;
+    private Image energyBar;
+    [SerializeField] private float currentEnergy = 50;
+    private float maxEnergy = 50;
+
+    private void Start()
     {
-        
+        // Set RigidBody:
+        rb = GetComponent<Rigidbody2D>();
+        //Get Health Bar Component:
+        healthBarGameObject = GameObject.FindWithTag("HealthBar");
+        healthBar = healthBarGameObject.GetComponent<Image>();
+        //Get Energy Bar Component:
+        energyBarGameObject = GameObject.FindWithTag("EnergyBar");
+        energyBar = energyBarGameObject.GetComponent<Image>();
+        //Set Variables:
+        currentMovementSpeed = walkingSpeed;
+        currentHealth =  maxHealth;
+        currentEnergy = maxEnergy;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        updatingMovement();
+        updatingHealthAndEnergy();
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = movementDirection * currentMovementSpeed;
+    }
+
+    // Updating Movement Speed: 
+    public void setWalkingSpeed(float newWalkingSpeed){walkingSpeed = newWalkingSpeed;}
+    public void setSprintSpeed(float newSprintSpeed){sprintSpeed = newSprintSpeed;}
+    private void updatingMovement()
     {
         
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        sprintSpeed = walkingSpeed + 5f;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if(currentEnergy != 0)
+                currentMovementSpeed = sprintSpeed;
+            else
+                currentMovementSpeed = walkingSpeed;
+            currentEnergy -= 50*Time.deltaTime;
+        }
+        else
+        {
+            currentMovementSpeed = walkingSpeed;
+            currentEnergy += 10*Time.deltaTime;
+        }
+        currentEnergy = Mathf.Clamp(currentEnergy, 0, maxEnergy);
+    }
+    private void updatingHealthAndEnergy()
+    {
+        healthBar.fillAmount = Mathf.Clamp(currentHealth / maxHealth, 0, 100);
+        energyBar.fillAmount = Mathf.Clamp(currentEnergy / maxEnergy, 0, 50);
     }
 }
